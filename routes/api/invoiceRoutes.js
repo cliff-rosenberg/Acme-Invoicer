6d632815e4a95b8ed3cef2018a8a2676f4539626
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-06-14 22:53:41 -0500

    adding outlines for customer, inventory, and invoices routes

---
+++ routes/api/invoiceRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+
+
+router.get('/', async (req, res) => {
+    // find invoices
+   
+  });
+  
+  router.get('/:id', async (req, res) => {
+    // find an invoice by its `id` value
+    
+  });
+  
+  router.post('/', async (req, res) => {
+    // create a new invoice
+  });
+  
+ module.exports = router;
