395a879e7db6e5a432ee6abb3cf8e483637bd0de
Cliff Rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-16 00:06:40 -0400

    corrected model for Customers

--- models/customer.js
+++ models/customer.js
@@ -1,32 +1,47 @@
-'use strict';
+const { Model, DataTypes } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
-  const Customer = sequelize.define('customer', {
+const sequelize = require('../config/connection');
+const Invoice = require('./invoice')
+
+// Model basics: https://sequelize.org/docs/v6/core-concepts/model-basics/
+class Customer extends Model {}
+
+Customer.init(
+  {
+    //define columns
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
       allowNull: false,
       unique: true
     },
-    companyName: {
+    company_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    contactName: {
+    contact_name: {
       type: DataTypes.STRING
     },
     address: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    phoneNumber: {
+    phone_number: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    emailAddress: {
+    email_address: {
       type: DataTypes.STRING
     }
-  }, { tableName: 'customer' });
+  },
+  {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'customer',
+  }
+);
 
-  return Customer;
-};
+  module.exports = Customer;
