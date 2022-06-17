b48fe2816b2e207b6ca170e40850510e3fb3be32
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-06-16 21:26:17 -0500

    server.js file and first get route added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
+
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/homeRoutes'));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT);
+});
