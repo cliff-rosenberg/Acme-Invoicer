b4ac5c893b70a081f91f8ba57151050e5ad08cb9
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-06-17 10:38:51 -0500

    updated inventoryRoutes.js

--- controllers/api/inventoryRoutes.js
+++ controllers/api/inventoryRoutes.js
@@ -1,23 +1,60 @@
 const router = require('express').Router();
+const { Inventory } = require('../../models');
 
 
-
+//find all products
 router.get('/', async (req, res) => {
-    // find all products
-   
-  });
+  try {
+      const inventoryData = await Inventory.findAll();
+          res.status(200).json(inventoryData);
+      
+  } catch (err) {
+      res.status(400).json(err);
+  }
+ 
+});
   
+  // find products by their `id` value
   router.get('/:id', async (req, res) => {
-    // find a product by its `id` value
+    console.log(req.params.id)
+    try {
+        const inventoryData = await Inventory.findByPk(req.params.id);
+        if (!inventoryData) {
+            res.status(404).json({ message: 'no product with this id' });
+            return;
+        }
+        res.status(200).json(inventoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
     
   });
   
+  // create a new product
   router.post('/', async (req, res) => {
-    // create a new product
+    try {
+        const inventoryData = await Inventory.create(req.body);
+        res.status(200).json(inventoryData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
   });
   
+  // update a product by its `id` value
   router.put('/:id', async (req, res) => {
-    // update a product by its `id` value
+    try {
+        const inventoryData = await Inventory.update(req.body, {
+            where: req.params.id
+        });
+        if (!inventoryData[0]) {
+            res.status(404).json({ message: 'no product with this id' });
+            return;
+        }
+        res.status(200).json(inventoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    
   });
 
   module.exports = router;
