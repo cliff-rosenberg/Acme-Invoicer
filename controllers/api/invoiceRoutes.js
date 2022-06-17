eaefab348a222993f7cb8448eb7331abaf6b12e7
Cliff Rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-17 17:56:42 -0400

    added route for Invoice details

--- controllers/api/invoiceRoutes.js
+++ controllers/api/invoiceRoutes.js
@@ -1,45 +1,71 @@
 const router = require('express').Router();
-const { Invoice } = require('../../models');
-const { Invoice_details } = require('../../models');
+const { Invoice, Customer } = require('../../models');
+const { Inventory, Invoice_details } = require('../../models');
 
 
 
 //find all invoices
 router.get('/', async (req, res) => {
   try {
-      const invoiceData = await Invoice.findAll();
-          res.status(200).json(invoiceData);
-      
+    // this will return all Invoice data in table
+    const invoiceData = await Invoice.findAll();
+    res.status(200).json(invoiceData);  
   } catch (err) {
-      res.status(400).json(err);
+    res.status(400).json(err);
   }
- 
 });
   
-  // find invoices by their `id` value
-  router.get('/:id', async (req, res) => {
-    console.log(req.params.id)
-    try {
-        const invoiceData = await Invoice.findByPk(req.params.id);
-        if (!invoiceData) {
-            res.status(404).json({ message: 'no invoice with this id' });
-            return;
-        }
-        res.status(200).json(invoiceData);
-    } catch (err) {
-        res.status(500).json(err);
+// find invoices by their `id` value
+router.get('/:id', async (req, res) => {
+  //console.log(req.params.id)
+  try {
+    // includes the data in the 'Customer' table,
+    // where Invoice Id has a matching Customer Id value in column
+    const invoiceData = await Invoice.findByPk(req.params.id, {
+      include: Customer,
+    });
+    // if NULL is returned, no invoice matching the given 'id' was found
+    if (!invoiceData) {
+        res.status(404).json({ message: 'no invoice found with this id' });
+        return;
     }
-    
-  });
+    res.status(200).json(invoiceData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// find invoices by their `id` value and include all details
+router.get('/details/:id', async (req, res) => {
+  //console.log(req.params.id)
+  try {
+    // includes the data in the 'Customer' table,
+    // where Invoice Id has a matching Customer Id value in column
+    const invoiceData = await Invoice.findByPk(req.params.id, {
+      include: {model: Invoice_details, required: true}
+    });
+    // if NULL is returned, no invoice matching the given 'id' was found
+    if (!invoiceData) {
+        res.status(404).json({ message: 'no invoice found with this id' });
+        return;
+    }
+    res.status(200).json(invoiceData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   
-  // create a new invoice
-  router.post('/', async (req, res) => {
-    try {
-        const invoiceData = await Invoice.create(req.body);
-        res.status(200).json(invoiceData);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-  });
+// create a new invoice
+router.post('/', async (req, res) => {
+  try {
+    // body must contain 'invoice_date' (in 'yyyy-mm-dd' format),
+    // and numeric 'customer_id' the invoice belongs to.
+    // the 'id' index for Invoice is auto-created by MySQL
+    const invoiceData = await Invoice.create(req.body);
+    res.status(200).json(invoiceData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
   
  module.exports = router;
