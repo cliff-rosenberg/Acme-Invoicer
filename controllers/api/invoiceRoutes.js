5dc382c69e8943f426bdad1fd5e35a694fb5616e
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-06-17 11:05:52 -0500

    updated invoice routes

--- controllers/api/invoiceRoutes.js
+++ controllers/api/invoiceRoutes.js
@@ -1,19 +1,45 @@
 const router = require('express').Router();
+const { Invoice } = require('../../models');
+const { Invoice_details } = require('../../models');
 
 
 
+//find all invoices
 router.get('/', async (req, res) => {
-    // find invoices
-   
-  });
+  try {
+      const invoiceData = await Invoice.findAll();
+          res.status(200).json(invoiceData);
+      
+  } catch (err) {
+      res.status(400).json(err);
+  }
+ 
+});
   
+  // find invoices by their `id` value
   router.get('/:id', async (req, res) => {
-    // find an invoice by its `id` value
+    console.log(req.params.id)
+    try {
+        const invoiceData = await Invoice.findByPk(req.params.id);
+        if (!invoiceData) {
+            res.status(404).json({ message: 'no invoice with this id' });
+            return;
+        }
+        res.status(200).json(invoiceData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
     
   });
   
+  // create a new invoice
   router.post('/', async (req, res) => {
-    // create a new invoice
+    try {
+        const invoiceData = await Invoice.create(req.body);
+        res.status(200).json(invoiceData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
   });
   
  module.exports = router;
