f5a28d7f1a2025bd2c123bd5646771ac45354c7b
Cliff Rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-18 00:57:45 -0400

    added route for User to login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+
 const customerRoutes = require("./customerRoutes");
 const inventoryRoutes = require("./inventoryRoutes");
 const invoiceRoutes = require("./invoiceRoutes");
+const userRoutes = require('./user-routes');
 
 router.use("/customer", customerRoutes);
 router.use("/inventory", inventoryRoutes);
 router.use("/invoice", invoiceRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
