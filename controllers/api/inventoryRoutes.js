bc8f4d5f3df94233b82c8de7fea2174e8ff27fc9
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-19 23:23:50 -0700

    updated routes for Handlebars

--- controllers/api/inventoryRoutes.js
+++ controllers/api/inventoryRoutes.js
@@ -6,17 +6,20 @@ const { Inventory } = require('../../models');
 router.get('/', async (req, res) => {
   try {
       const inventoryData = await Inventory.findAll();
-          res.status(200).json(inventoryData);
-      
-  } catch (err) {
-      res.status(400).json(err);
-  }
- 
+      let rendered = inventoryData.map((data) => data.get({ plain: true }));
+      console.log(rendered);
+      res.render('inventory', {
+        rendered,
+        });
+      //res.status(200).json(inventoryData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
 });
   
   // find products by their `id` value
   router.get('/:id', async (req, res) => {
-    console.log(req.params.id)
     try {
         const inventoryData = await Inventory.findByPk(req.params.id);
         if (!inventoryData) {
