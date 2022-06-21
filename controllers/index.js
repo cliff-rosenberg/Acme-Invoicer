0f9d3c90a11ff23b44e60a83281813d8e957d687
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-21 10:37:03 -0700

    added code comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
-const apiRoutes = require("./api")
 
+// set up routes here
+const apiRoutes = require("./api")
 const homeRoutes = require('./homeRoutes.js');
 
+// the "Home Routes" do not need the "/api" prefix
 router.use('/', homeRoutes);
 
+// the "API Routes" need the prefix of "/api" to work
 router.use('/api', apiRoutes);
 
 module.exports = router;
