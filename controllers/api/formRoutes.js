24b3c42ee4b89280f01e598a3c3342e20c111f96
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-21 16:15:17 -0700

    added Customer delete route

--- controllers/api/formRoutes.js
+++ controllers/api/formRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/auth');
+const { Customer } = require('../../models');
 
 // add Customer form
 router.get('/customerform', withAuth, async (req, res) => {
@@ -12,8 +13,35 @@ router.get('/customerform', withAuth, async (req, res) => {
     }
 });
 
+// delete a Customer form
+router.get('/delcustomerform', withAuth, async (req, res) => {
+    try {
+        const customerData = await Customer.findAll();
+            //res.status(200).json(customerData);
+            // serialize the Sequelize query return
+            const rendered = customerData.map((data) => data.get({ plain: true }));
+            console.log(rendered);
+        res.render('custDelete', {
+            rendered,
+            logged_in: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // add Inventory item form
 router.get('/inventoryform', withAuth, async (req, res) => {
+    try {
+        res.render('inventoryCreate', {
+            logged_in: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/delinventoryform', withAuth, async (req, res) => {
     try {
         res.render('inventoryCreate', {
             logged_in: req.session.loggedIn,
