ee106a82ce0aa8cba76b62c70d4c4db8f9711ab2
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-20 22:11:40 -0700

    Update invoice.js

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -1,3 +1,4 @@
+// function to do POST to NodeMailer
   const sendInvoice = async (data) => {
     // event.preventDefault();
       const response = await fetch('/api/emails/invoice/' + data, {
@@ -9,15 +10,15 @@
       if (response.ok) {
         document.location.replace('/');
       } else {
+        console.log('Failed to send invoice.');
         alert('Failed to send invoice.');
       }
     };
 
-
+// JQuery to make buttons work for passing invoice ID into function
   $( ".invoice-btn" ).on( "click", function() {
-    console.log( $( this ).data( "invoiceid" ));
+    //console.log( $( this ).data( "invoiceid" ));
     let temp =  $( this ).data( "invoiceid" );
     console.log(temp);
-    // console.log(this);
     sendInvoice(temp);
   });
