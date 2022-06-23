df67427ed243ed4827f4b7c4aaedd26b76def06a
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-22 22:19:17 -0700

    added comments, fixed code formatting

--- controllers/api/formRoutes.js
+++ controllers/api/formRoutes.js
@@ -1,74 +1,117 @@
-const router = require('express').Router();
-const withAuth = require('../../utils/auth');
+//*
+//* These are the Express routes to load 
+//* various HTML data input forms
+//* all routes have the '/api/forms' prefix in the URL
+//*
+// require Customer and Inventory models
 const { Customer, Inventory } = require('../../models');
+// set up Express router
+const router = require('express').Router();
+// load 'auth.js' util
+const withAuth = require('../../utils/auth');
 
-// add Customer form
+//* Express route for Add Customer form
 router.get('/customerform', withAuth, async (req, res) => {
     try {
+        // render form using Handlebars
         res.render('custCreate', {
             logged_in: req.session.loggedIn,
         });
     } catch (err) {
+        // returns a Server error response
         res.status(500).json(err);
     }
 });
 
-// delete a Customer form
+//* Express route for Delete Customer form
 router.get('/delcustomerform', withAuth, async (req, res) => {
     try {
+        // search the database for all current Customer data
         const customerData = await Customer.findAll();
-            //res.status(200).json(customerData);
-            // serialize the Sequelize query return
-            const rendered = customerData.map((data) => data.get({ plain: true }));
-            console.log(rendered);
+        // serialize the Sequelize query return
+        const rendered = customerData.map((data) => data.get({ plain: true }));
+        //console.log(rendered);
+        // render form using Handlebars
         res.render('custDelete', {
             rendered,
             logged_in: req.session.loggedIn,
         });
     } catch (err) {
+        // returns a Server error response
         res.status(500).json(err);
     }
 });
 
-// delete a Customer form
+//* Express route for displaying a single Customer form
 router.get('/findcustform', withAuth, async (req, res) => {
     try {
+        // search the database for all current Customer data
+        // to populate the drop down input selector
         const customerData = await Customer.findAll();
-            //res.status(200).json(customerData);
-            // serialize the Sequelize query return
-            const rendered = customerData.map((data) => data.get({ plain: true }));
-            console.log(rendered);
+        // format the returned JSON
+        const rendered = customerData.map((data) => data.get({ plain: true }));
+        //console.log(rendered);
+        // render form using Handlebars
         res.render('custFind', {
             rendered,
             logged_in: req.session.loggedIn,
         });
     } catch (err) {
+        // returns a Server error response
         res.status(500).json(err);
     }
 });
 
-// add Inventory item form
+//* Express route for displaying a single Inventory item form
+router.get('/findinvform', withAuth, async (req, res) => {
+    try {
+        // search the database for all current Inventory items
+        // to populate the drop down input selector
+        const inventoryData = await Inventory.findAll();
+        // format the returned JSON
+        const rendered = inventoryData.map((data) => data.get({ plain: true }));
+        console.log(rendered);
+        // render form using Handlebars
+        res.render('inventoryFind', {
+            rendered,
+            logged_in: req.session.loggedIn,
+        });
+    } catch (err) {
+        // returns a Server error response
+        res.status(500).json(err);
+    }
+});
+
+//* Express route for adding an Inventory item form
 router.get('/inventoryform', withAuth, async (req, res) => {
     try {
+        // render Inventory item input form
+        // using Handlebars
         res.render('inventoryCreate', {
             logged_in: req.session.loggedIn,
         });
     } catch (err) {
+        // returns a Server error response
         res.status(500).json(err);
     }
 });
 
-// delete a Inventory item
+//* Express route for deleting a single Inventory item form
 router.get('/delinventoryform', withAuth, async (req, res) => {
     try {
+        // search the database for all current Inventory item data
+        // to populate the drop down input selector
         const inventoryData = await Inventory.findAll();
+        // format the returned JSON
         const rendered = inventoryData.map((data) => data.get({ plain: true }));
-        console.log(rendered);
+        //console.log(rendered);
+        // render form using Handlebars
         res.render('inventoryDel', {
             rendered,
             logged_in: req.session.loggedIn,
         });
     } catch (err) {
+        // returns a Server error response
         res.status(500).json(err);
     }
 });
