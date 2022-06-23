2fa11534bcdd183bbca8510c9c7299212555db5a
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-22 22:20:11 -0700

    fixed code formatting and added comments

--- controllers/api/inventoryRoutes.js
+++ controllers/api/inventoryRoutes.js
@@ -1,48 +1,64 @@
-const router = require('express').Router();
+//*
+//* These are the Express routes for Inventory functions
+//* all routes have the '/api/inventory' prefix in the URL
+//*
+// require Inventory model
 const { Inventory } = require('../../models');
+// set up Express router
+const router = require('express').Router();
 
-
-//find all products
+//* Express route to find all Inventory items
 router.get('/', async (req, res) => {
   try {
+    // retrieve all Inventory items in database
       const inventoryData = await Inventory.findAll();
-      //res.status(200).json(inventoryData);
+      // format the returned JSON
       const rendered = inventoryData.map((data) => data.get({ plain: true }));
       console.log(rendered);
       res.render('inventory', {
+        tblCaption: 'All Inventory Items',
         rendered,
         logged_in: req.session.loggedIn,
         });
     } catch (err) {
-        console.log(err);
+        // returns a Client error response
         res.status(400).json(err);
     }
 });
-  
-  // find products by their `id` value
-  router.get('/:id', async (req, res) => {
+
+//* Express route to find Inventory items by their `inventory_id` value
+router.post('/find', async (req, res) => {
+    console.log(req.body);
     try {
-        const inventoryData = await Inventory.findByPk(req.params.id);
+        const body = req.body;
+        // search the databse for relevant Inventory item data
+        const inventoryData = await Inventory.findAll({
+            where: {
+                inventory_id: body.dataval
+            }
+        });
         if (!inventoryData) {
             res.status(404).json({ message: 'no product with this id' });
             return;
         }
-        //res.status(200).json(inventoryData);
+        // format the returned JSON
         const rendered = inventoryData.map((data) => data.get({ plain: true }));
-        console.log(rendered);
+        //console.log(rendered);
+        // render using Handlebars
         res.render('inventory', {
+            tblCaption: 'Single Inventory Item',
             rendered,
             logged_in: req.session.loggedIn,
         });
     } catch (err) {
+        // returns a Server error response
         res.status(500).json(err);
     }
-    
-  });
+});
   
-  // create a new product
-  router.post('/', async (req, res) => {
-    console.log(req.body);
+//* Express route to create a new Inventory item
+router.post('/', async (req, res) => {
+    //console.log(req.body);
     try {
         if (!req.body.item_name) {
             res.render('error', {
@@ -56,28 +72,13 @@ router.get('/', async (req, res) => {
         res.status(200).json(inventoryData);
         };
     } catch (err) {
+        // returns a Client error response
         res.status(400).json(err);
     }
-  });
+});
   
-  // update a product by its `id` value
-  router.put('/:id', async (req, res) => {
-    try {
-        const inventoryData = await Inventory.update(req.body, {
-            where: req.params.id
-        });
-        if (!inventoryData[0]) {
-            res.status(404).json({ message: 'no product with this id' });
-            return;
-        }
-        res.status(200).json(inventoryData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-    
-  });
-
-  router.post('/delete', async (req, res) => {
+//* Express route to delete a single Inventory item from Database
+router.post('/delete', async (req, res) => {
     console.log(req.body);
     try {
         const body = req.body;
@@ -89,11 +90,29 @@ router.get('/', async (req, res) => {
         const invUpdated = await Inventory.findAll();
         const rendered = invUpdated.map((data) => data.get({ plain: true }));
         res.render('inventory', {
+            tblCaption: 'Updated Inventory Items',
             rendered,
             logged_in: req.session.loggedIn,
             });
-        //res.status(200).json({message: 'Deleted OK'});
     } catch (err) {
+        // returns a Server error response
+        res.status(500).json(err);
+    }
+});
+
+// TODO: Express route to update an Inventory item by its `inventory_id` value
+router.put('/:id', async (req, res) => {
+    try {
+        const inventoryData = await Inventory.update(req.body, {
+            where: req.params.id
+        });
+        if (!inventoryData) {
+            res.status(404).json({ message: 'no product with this id' });
+            return;
+        }
+        res.status(200).json(inventoryData);
+    } catch (err) {
+        // returns a Server error response
         res.status(500).json(err);
     }
 });
